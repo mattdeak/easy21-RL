a354b7bb4db96edd7940b8fa7f816f7c990e3916
matthew <mwoolnerdeakos@gmail.com>
2017-03-30 22:18:45 -0400

    initial commiet

---
+++ environment.py
@@ -0,0 +1,18 @@
+import dealer
+from player import Player
+
+class Environment:
+
+
+	#Inner class dealer
+	class Dealer:
+
+	def draw(self):
+		
+		
+	
+	player = Player()
+
+	def step(self,state):
+
+	
